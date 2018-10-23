33dd1b6b1949c57cab374f338bc7baf630130b85
michaelrodgers <38140530+michaelrodgers@users.noreply.github.com>
2018-10-22 20:29:34 -0700

    Add files via upload

--- template1.php
+++ template1.php
@@ -1,5 +1,8 @@
         <?php include "header1.php"?>
         
-<div class="section"><div class="w-container"><div class="section-title-group"><h2 class="centered section-heading">Section Heading</h2><div class="center sasd-asd-as section-subheading">This is some text inside of a div block.</div></div><div class="w-row"><div class="w-col w-col-6"><div class="w-row"><div class="w-col w-col-4"><img class="grid-image" src="https://d3e54v103j8qbb.cloudfront.net/img/image-placeholder.svg" width="100"></div><div class="w-col w-col-8"><h3>Heading</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p></div></div></div><div class="w-col w-col-6"><div class="w-row"><div class="w-col w-col-4"><img class="grid-image" src="https://d3e54v103j8qbb.cloudfront.net/img/image-placeholder.svg" width="100"></div><div class="w-col w-col-8"><h3>Heading</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p></div></div></div></div><div class="w-row"><div class="w-col w-col-6"><div class="w-row"><div class="w-col w-col-4"><img class="grid-image" src="https://d3e54v103j8qbb.cloudfront.net/img/image-placeholder.svg" width="100"></div><div class="w-col w-col-8"><h3>Heading</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p></div></div></div><div class="w-col w-col-6"><div class="w-row"><div class="w-col w-col-4"><img class="grid-image" src="https://d3e54v103j8qbb.cloudfront.net/img/image-placeholder.svg" width="100"></div><div class="w-col w-col-8"><h3>Heading</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p></div></div></div></div></div></div>
+<h1>Clever Content Goes Here</h1>
+    <p>Content</p>
+    <p>Content</p>
+    <p>Content</p>
         
         <?php include 'footer1.php'?>
